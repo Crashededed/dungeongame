a5c3cf03d74af298d816d5fb23dd723be7daff8f
Crashededed <39970319+Crashededed@users.noreply.github.com>
2018-09-23 15:01:18 +0300

    Initial commit

---
+++ game2.py
@@ -0,0 +1,36 @@
+def start():
+    # add descriptions etc.
+    print("""start of adventure!\n\n""")
+    cave()
+
+def cave():
+    # cave
+    print("you wake up in a dark cave\nyou lay on an ancient altar")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+start()
