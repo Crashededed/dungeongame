1d2e87a9fca4e92e9b43573b138f1384d2a480a1
Crashededed <39970319+Crashededed@users.noreply.github.com>
2018-09-23 17:06:49 +0300

    finished the first room(cave)

--- game2.py
+++ game2.py
@@ -1,16 +1,17 @@
 def start():
     # add descriptions etc.
-    print("""start of adventure!\n\n""")
+    key = 0
+    print("""start of adventure!\n""")
     cave()
 
 
 def prompt():
-    x = int(input("Type a command: "))
     x = int(input("\nType a command: "))
     return x
 
 def cave():
     # cave
+
     print("you wake up in a dark cave,\nyou lay on an ancient altar,"
           "\nthere's a dim light coming from a hole in the wall\n"
           "1.examine the altar.\n2.examine the hole")
@@ -19,8 +20,9 @@ def cave():
         altar()
     elif command == 2:
         hole()
+    else:
+        cave()
 
-def hole():
 def cave1():
     # cave
     print("you are in a dark cave, you see an ancient altar,"
@@ -31,6 +33,8 @@ def cave1():
         altar()
     elif command == 2:
         hole()
+    else:
+        cave1()
 
 
 def hole():
@@ -39,11 +43,34 @@ def hole():
           "2.back off the hole\n")
     command = prompt()
     if command == 1:
-        print("you find a key inside the hole, it's shining brightly\n\n")
-        key="true"
+        print("you find a key inside the hole, it's shining brightly\n")
+        global key
+        key = 1
         cave1()
     elif command == 2:
         cave1()
+    else:
+        hole()
 
+def altar():
+    print("you examine the altar.\nit is covered with markings and stains.\n"
+          "its has a large hole in the middle of it"
+          "\n1.examine the hole."
+          "\n2.back off the altar")
+    command=prompt()
+    if command == 1:
+        if key == 1:
+            print("you try the key you found in the hole\n"
+                  "the altar rises into the ceiling and reveals a staircase below it\n")
+            staircase()
+        else:
+            print("you don't have anything to put in the hole.\nyou examine the altar again\n")
+            altar()
 
+    elif command == 2:
+        cave1()
+    else:
+        altar()
+def staircase():
+    print("below the altar appears a staircase")
 start()
