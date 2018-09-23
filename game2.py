84712e8389f397a0f0311b0c69ccb0a6c9333dfe
Crashededed <39970319+Crashededed@users.noreply.github.com>
2018-09-23 15:24:12 +0300

    Revert "Revert "Initial commit""
    This reverts commit 29bab88aa8cfc6dcc7d186fd38d0f1575ebdd080.

--- game2.py
+++ game2.py
@@ -3,9 +3,24 @@ def start():
     print("""start of adventure!\n\n""")
     cave()
 
+
+def prompt():
+    x = int(input("Type a command: "))
+    return x
+
 def cave():
     # cave
-    print("you wake up in a dark cave\nyou lay on an ancient altar")
+    print("you wake up in a dark cave,\nyou lay on an ancient altar,"
+          "\nthere's a dim light coming from a hole in the wall\n"
+          "1.examine the altar.\n2.examine the hole")
+    command = prompt()
+    if command == 1:
+        altar()
+    elif command == 2:
+        hole()
+
+
+
 
 
 
